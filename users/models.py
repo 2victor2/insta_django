0bd84634df2a93e30d040a31c7eb461d09daa935
Victor Ramon <91630027+2victor2@users.noreply.github.com>
2022-09-20 21:51:28 -0300

    refactor: modified default fields for user

--- users/models.py
+++ users/models.py
@@ -1,16 +1,17 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from .utils import CustomUserManager
 from django.db import models
 import uuid
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     private_profile = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
-    REQUIRED_FIELDS = ["name", "password", "private_profile"]
+    REQUIRED_FIELDS = ["name", "private_profile"]
     USERNAME_FIELD = "email"
     objects = CustomUserManager()
