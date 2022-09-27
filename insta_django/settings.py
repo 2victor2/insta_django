576c9165422f38e18e06073081a7ab8c66bded3f
Victor Ramon <91630027+2victor2@users.noreply.github.com>
2022-09-27 12:32:26 -0300

    build: updated configs to deploy

--- insta_django/settings.py
+++ insta_django/settings.py
@@ -28,9 +28,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv("DEBUG", 0)))
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.getenv("ALLOWED_HOSTS", "").split(","),
+    )
+)
 
 
 # Application definition
